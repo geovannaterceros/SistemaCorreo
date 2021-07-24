96b2c2e4ffd2903b25227bb325f444e2c665d22a
Geovanna Gil <geovannaterceros@gmail.com>
2021-07-24 12:25:54 -0400

    Iniciando los Archivos del proyecto

---
+++ SistemaCorreo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaCorreo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
